a8ce920abae808addb6bab1a9f669aaa7d11093e
chs <chaeheesung33@gmail.com>
2024-01-18 16:29:59 +0900

    백준 알고리즘 연습

---
+++ src/Main10757Baekjoon.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main10757Baekjoon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextBigInteger().add(sc.nextBigInteger()));
+    }
+}
