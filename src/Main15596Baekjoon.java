d0e2c463c06dd38b30b635608936856c63c40abe
chs <chaeheesung33@gmail.com>
2024-01-19 09:51:11 +0900

    백준 알고리즘 연습

---
+++ src/Main15596Baekjoon.java
@@ -0,0 +1,11 @@
+public class Main15596Baekjoon {
+    public class Test {
+        long sum(int[] a) {
+            long ans = 0;
+            for (int i = 0; i < a.length; i++) {
+                ans += a[i];
+            }
+            return ans;
+        }
+    }
+}
