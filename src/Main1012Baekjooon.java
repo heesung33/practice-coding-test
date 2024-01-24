5d500d50375fc672c9cf5535d1ad8a689426f543
chs <chaeheesung33@gmail.com>
2024-01-24 10:38:15 +0900

    백준 알고리즘 연습 1012번(해결)

--- src/Main1012Baekjooon.java
+++ src/Main1012Baekjooon.java
@@ -4,44 +4,64 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main1012Baekjooon {
+    static boolean[][] visited;
+    static int[][] map;
+    static int n;
+    static int m;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int r = Integer.parseInt(st.nextToken());
         for (int i = 0; i < r; i++) {
             st = new StringTokenizer(br.readLine());
-            int M = Integer.parseInt(st.nextToken());
-            int N = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
-            boolean[] visited = new boolean[M];
-            ArrayList<Integer>[] arrayList = new ArrayList[M];
-            for (int j = 0; j < arrayList.length; j++) {
-                arrayList[j] = new ArrayList<>();
-            }
-            for (int j = 0; j < K; j++) {
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            map = new int[n][m];
+            visited = new boolean[n][m];
+            for (int j = 0; j < k; j++) {
                 st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
-                arrayList[a].add(b);
-                arrayList[b].add(a);
+                map[b][a] = 1;
             }
-            bfs(visited, arrayList, 0);
-            System.out.println();
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                for (int c = 0; c < m; c++) {
+                    if(visited[j][c] || map[j][c] == 0) {
+                        continue;
+                    }
+                    bfs(j, c);
+                    count++;
+                }
+            }
+            System.out.println(count);
         }
     }
 
-    public static void bfs(boolean[] visited, ArrayList<Integer>[] arrayList, int v) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(v);
-        visited[v] = true;
+    public static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
         while (!q.isEmpty()) {
-            v = q.poll();
-            System.out.print(v + " ");
-            for (int i : arrayList[v]) {
-                if (!visited[i]) {
-                    q.add(i);
-                    visited[i] = true;
+            int now[] = q.poll();
+            int nowX = now[0];
+            int nowY = now[1];
+            for (int i = 0; i < 4; i++) {
+                int nextX = nowX + dx[i];
+                int nextY = nowY + dy[i];
+                if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) {
+                    continue;
                 }
+                if (visited[nextX][nextY] || map[nextX][nextY] == 0) {
+                    continue;
+                }
+                map[nextX][nextY] = map[nowX][nowY] + 1;
+                q.add(new int[]{nextX, nextY});
+                visited[nextX][nextY] = true;
             }
         }
     }
