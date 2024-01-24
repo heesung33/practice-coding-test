c1ab61d3a6c9249d0aa16a8222603c2a1eac60be
chs <chaeheesung33@gmail.com>
2024-01-24 09:51:56 +0900

    백준 알고리즘 연습 2178번(해결)

--- src/Main2178Baekjoon.java
+++ src/Main2178Baekjoon.java
@@ -1,5 +1,56 @@
-public class Main2178Baekjoon {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+public class Main2178Baekjoon {
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static int n;
+    static int m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = s.charAt(j) - '0';
+            }
+        }
+        visited = new boolean[n][m];
+        bfs(0, 0);
+        System.out.print(map[n - 1][m - 1]);
+    }
+
+    public static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
+        while (!q.isEmpty()) {
+            int now[] = q.poll();
+            int nowX = now[0];
+            int nowY = now[1];
+            for (int i = 0; i < 4; i++) {
+                int nextX = nowX + dx[i];
+                int nextY = nowY + dy[i];
+                if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) { // 상하좌우 움직였을 때 배열 탈출했을 시 넘어감
+                    continue;
+                }
+                if(visited[nextX][nextY] || map[nextX][nextY] == 0) { // 이미 방문했거나 0이면 넘어감
+                    continue;
+                }
+                map[nextX][nextY] = map[nowX][nowY] + 1; // 다음으로 이동할 위치에 1을 더해줌.
+                q.add(new int[] {nextX, nextY});
+                visited[nextX][nextY] = true;
+            }
+        }
     }
 }
